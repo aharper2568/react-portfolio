c09b7dbbbf88bf71b18a6d04f0451e0432a17405
aharper2568 <anthonyharper568@gmail.com>
2024-09-23 15:16:00 -0400

    added filler text to about component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,13 @@
 export default function About() {
-  
+  return (
+    <>
+      <h1> About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat harum
+        quia soluta laudantium esse tempora. Eligendi, nam consequuntur ullam in
+        id corporis modi laborum dignissimos temporibus dolor et. Numquam,
+        cumque.
+      </p>
+    </>
+  );
 }

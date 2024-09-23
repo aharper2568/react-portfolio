983148e1a73e4f0326b97969dcdd82a1b9d11c15
aharper2568 <anthonyharper568@gmail.com>
2024-09-23 15:03:43 -0400

    updated main.jsx for routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,43 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import Resume from './pages/Resume';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import About from './pages/About';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume/>,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio/>,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);

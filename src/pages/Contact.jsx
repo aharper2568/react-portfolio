8a9f06111da0efe600f646524659cd8d1526d241
aharper2568 <anthonyharper568@gmail.com>
2024-09-23 17:38:12 -0400

    added forms with email validation and handler functions

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,62 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!formData.name || !formData.email || !formData.message) {
+      setErrorMessage("All fields are required");
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      setErrorMessage("Invalid email address");
+    } else {
+      setErrorMessage("");
+      console.log("Form submitted:", formData);
+    }
+  };
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
+    <section className="form-group" id="contact">
+      <h2>Contact Me</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form-control m-1"
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          onChange={handleInputChange}
+          value={formData.name}
+        />
+        <input
+          // type="email"
+          className="form-control m-1"
+          name="email"
+          placeholder="Your Email"
+          onChange={handleInputChange}
+          value={formData.email}
+        />
+        <textarea
+          className="form-control m-1"
+          name="message"
+          placeholder="Your Message"
+          onChange={handleInputChange}
+          value={formData.message}
+        />
+        <button className="btn btn-success" type="submit">
+          Send
+        </button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </section>
   );
 }

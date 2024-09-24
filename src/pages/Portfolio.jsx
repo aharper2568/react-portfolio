51ed182c5be1a0e26e8c028dc0781dbce02354e3
aharper2568 <anthonyharper568@gmail.com>
2024-09-24 15:39:55 -0400

    imported images, array of project objects to map over and render projects dynamically

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,3 +1,64 @@
+import Project from "../components/Projects";
+import ECBS from "../assets/ECBS.png"
+import PRT from "../assets/PRT.png"
+import NTS from "../assets/NTS.png"
+import RMG from "../assets/RMG.png"
+import WBP from "../assets/WBP.png"
+import STP from "../assets/STP.png"
+
 export default function Portfolio() {
-  
-}
+  const projects = [
+    {
+      title: "E-Commerce Back-End Server",
+      image: ECBS,
+      githubLink: "https://github.com/aharper2568/E-Commerce-Back-End-Server",
+      liveLink: "https://github.com/aharper2568/E-Commerce-Back-End-Server",
+    },
+    {
+      title: "Employee Payroll Tracker",
+      image: PRT,
+      githubLink: "https://github.com/aharper2568/payroll-tracker",
+      liveLink: "https://aharper2568.github.io/payroll-tracker/",
+    },
+    {
+      title: "Note Taking App on a Server",
+      image: NTS,
+      githubLink: "https://github.com/aharper2568/note-taker-server",
+      liveLink: "https://note-taker-server-9tay.onrender.com/",
+    },
+    {
+      title: "Node Based README Generator",
+      image: RMG,
+      githubLink: "https://github.com/aharper2568/readme-generator",
+      liveLink: "https://github.com/aharper2568/readme-generator",
+    },
+    {
+      title: "Online Pokedex",
+      image: WBP,
+      githubLink: "https://github.com/aharper2568/web-pokedex",
+      liveLink: "https://aharper2568.github.io/web-pokedex/",
+    },
+    {
+      title: "Online Task-Board w/ local storage saving",
+      image: STP,
+      githubLink: "https://github.com/aharper2568/simple-taskboard",
+      liveLink: "https://aharper2568.github.io/web-pokedex/",
+    },
+  ];
+  return (
+    <div>
+      <h1>My Projects</h1>
+      <div className="project-list">
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            title={project.title}
+            image={project.image}
+            githubLink={project.githubLink}
+            liveLink={project.liveLink}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};

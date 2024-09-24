c4b78d7aad1891ec94f438d552ad2632a3e2e16a
aharper2568 <anthonyharper568@gmail.com>
2024-09-24 14:18:37 -0400

    inserted bootswatch style import

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 // import './index.css'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Resume from './pages/Resume';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import About from './pages/About';
+import Resume from "./pages/Resume";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import "bootswatch/dist/lux/bootstrap.min.css";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     // errorElement: <Error />,
     children: [
@@ -24,21 +23,21 @@ const router = createBrowserRouter([
         element: <About />,
       },
       {
-        path: '/Resume',
-        element: <Resume/>,
+        path: "/Resume",
+        element: <Resume />,
       },
       {
-        path: '/Portfolio',
-        element: <Portfolio/>,
+        path: "/Portfolio",
+        element: <Portfolio />,
       },
       {
-        path: '/Contact',
+        path: "/Contact",
         element: <Contact />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );

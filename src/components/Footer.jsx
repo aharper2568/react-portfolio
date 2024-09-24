a2bf7ef87dcdd9790ddc1dd5b8505dd714b953a7
aharper2568 <anthonyharper568@gmail.com>
2024-09-24 14:14:08 -0400

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+
+  <footer className="container nav nav-tabs text-center mt-5 justify-content-center w-100">
+    <a className="m-2" href="#">Github</a>
+    <a className="m-2" href="#">Github</a>
+    <a className="m-2" href="#">Github</a>
+  </footer>
+  );
+}

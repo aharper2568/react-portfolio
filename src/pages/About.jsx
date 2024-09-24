ed6fbe5774e03807f69a6b08af549a975572b239
aharper2568 <anthonyharper568@gmail.com>
2024-09-24 15:40:16 -0400

    added content to About Me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,14 @@
 export default function About() {
   return (
     <>
-      <h1> About Me</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat harum
-        quia soluta laudantium esse tempora. Eligendi, nam consequuntur ullam in
-        id corporis modi laborum dignissimos temporibus dolor et. Numquam,
-        cumque.
+<div className="container w-50">
+
+      <h1 > About Me</h1>
+        <img src="https://www.shareicon.net/data/128x128/2016/05/29/772559_user_512x512.png" alt="random avatar" />
+      <p className="my-3 mb-5">
+        Hello! My name is <b>Anthony Harper</b>. I'm a student in an EDx course for full-stack web development. So far I've learned a lot about HTML,CSS, and JavaScript for the front-end and back-end. So far I've had a lot of fun on my learning journey and hope to further my knowledge going forward. I have interests in developing mobile applications and working with health-related apps.
       </p>
+</div>
     </>
   );
 }

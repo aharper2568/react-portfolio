fcef3d69dd30d896a00f1abd8570b2d3e1b6bdae
aharper2568 <anthonyharper568@gmail.com>
2024-09-25 14:00:25 -0400

    added resume content with styles

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,3 +1,25 @@
 export default function Resume() {
-  
+  return (
+    <section id="resume">
+      <h2>Resume</h2>
+      <section id="resume-card" className="container border">
+        <h3>Proficiencies</h3>
+        <ul className="list-group">
+          <li className="list-group-item list-group-item-success">
+            JavaScript
+          </li>
+          <li className="list-group-item list-group-item-primary">
+            PostgreSQL
+          </li>
+          <li className="list-group-item list-group-item list-group-item-success">
+          React
+          </li>
+          <li className="list-group-item list-group-item-primary">
+          MongoDB
+          </li>
+        </ul>
+      </section>
+      <a href="#" className="link-offset-2 link-underline link-underline-opacity-0" download>Download Resume Here</a>
+    </section>
+  )
 }

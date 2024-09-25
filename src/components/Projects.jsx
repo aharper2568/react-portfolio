9b4b4e210ca4b321a5c47a9675cd21a8f8685e5f
aharper2568 <anthonyharper568@gmail.com>
2024-09-25 15:17:59 -0400

    fixed styling with mobile-first design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,10 @@
 const Project = ({ title, image, githubLink, liveLink }) => {
   return (
-    <div className="project border mb-2 w-50 col-md-4 d-flex flex-column">
-      <h3>{title}</h3>
+    <div className="project border mb-2 w-50 col-md-4 d-flex flex-column hover-shadow">
+      <h3 className="title">{title}</h3>
       <img src={image} className="w-100" alt={`${title} Screenshot`} />
       <div className="mt-auto">
-        <p>
+        <p className="title">
           <a href={githubLink} className="link-offset-2 link-underline link-underline-opacity-0" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
